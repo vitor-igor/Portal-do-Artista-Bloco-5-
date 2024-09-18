01b482b63b0361af510849e82674ce75c3ac871f
Vitor Igor <140018537+vitor-igor@users.noreply.github.com>
2024-09-18 06:46:39 -0300

    Atualizar o index.js

--- public/index.js
+++ public/index.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch('/posts')
         .then(response => response.json())
         .then(data => {
-            renderPosts(data);  // Render initial posts
+            renderPosts(data);  
 
             // Event listener for the search bar
             searchBar.addEventListener('keyup', () => {
